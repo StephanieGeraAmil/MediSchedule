228e3d6b33761c65be4a9732d6b24181817057c6
StephanieGeraAmil <stephanie.gera.amil@gmail.com>
2024-11-06 17:44:23 -0300

    adding basic login form

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import PatientForm from "@/components/forms/PatientForm";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return(
@@ -15,13 +16,21 @@ export default function Home() {
       className="mb-12 h-10 w-fit"
       />
       <PatientForm/>
-      <div className="text-14-regular mt-20 flex justify-betweem">
+      <div className="text-14-regular mt-20 flex justify-between">
         <p className="justify-items-end text-dark-600 xl:text-left">
            © 2024 MediSchedule
-        </p>
+            </p>
+            <Link href="/?admin=true" className="text-green-500">Admin</Link>
         </div>
     </div>
-   </section>
+      </section>
+      <Image
+         src="/assets/images/onboarding-img.png"
+         height={1000}
+         width={1000}
+         alt="health-team-photo"
+         className="side-img max-w-[50%]"
+      />
     </div>
   )
 }
